bc5491dc6d660536ea881576d414e913c486a242
Guilherme <guilherme.640518@outlook.com>
2026-04-07 15:02:19 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,10 +31,18 @@ public class Program {
         for (Seller obj : list){
             System.out.println(obj);
         }
+
         System.out.println("\n=== TEST 4: seller insert ===");
         Seller newSeller = new Seller (null, "Greg", "greg@gmail.com", LocalDate.now(), 4000.0, new Department(2, null));
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New Id : " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update Completed");
+
 
     }
 }
