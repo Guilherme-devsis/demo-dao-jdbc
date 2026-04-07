cc40dd71d3d628a64a3209f2db16ad13debb57bd
Guilherme <guilherme.640518@outlook.com>
2026-04-07 08:46:22 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,23 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Program {
     static void main(String[] args) {
 
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate birthDate = LocalDate.parse("15/10/1995", fmt);
+
         Department obj = new Department(1, "Books");
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", birthDate,3000.0, obj);
+
         System.out.println(obj);
+        System.out.println(seller);
 
     }
 }
